eac01d0cf509b54b7e168b1968860d9956b204e2
vamshi <alugojuvamshi@gmail.com>
2023-06-30 21:41:48 +0530

    modal functionality added

--- src/App.js
+++ src/App.js
@@ -1,15 +1,23 @@
+import React from "react";
 import Header from "./components/Layout/Header";
 import Summary from "./components/Layout/Summary";
 import Meals from "./components/Meals/Meals";
 import OrderModal from "./components/Meals/OrderModal";
 
 function App() {
+  const [isshown,setisshown] = React.useState(false);
+
+  function Handleisshown()
+  {
+      setisshown(!isshown);
+  }
+
   return (
     <div>
-      <Header />
+      <Header handleCart={Handleisshown} />
       <Summary />
       <Meals />
-      <OrderModal />
+     { isshown && <OrderModal handleCart={Handleisshown} />}
     </div>
   );
 }
