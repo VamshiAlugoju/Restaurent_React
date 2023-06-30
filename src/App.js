5693ac1faf50503fe68403d69afd8f3b277c5c00
vamshi <alugojuvamshi@gmail.com>
2023-06-30 19:54:14 +0530

    list added

--- src/App.js
+++ src/App.js
@@ -1,10 +1,13 @@
 import Header from "./components/Layout/Header";
 import Summary from "./components/Layout/Summary";
+import Meals from "./components/Meals/Meals";
+
 function App() {
   return (
     <div>
       <Header />
       <Summary />
+      <Meals />
     </div>
   );
 }
